fe0171e9287dfcc1c1f4112586fd4d420e6b8915
wuyeelok <wuyeelok@gmail.com>
2024-02-14 09:52:31 +0800

    varaibables and constants

--- sandbox/01-variables-data-types/02-comments-shortcuts/script.js
+++ sandbox/01-variables-data-types/02-comments-shortcuts/script.js
@@ -6,3 +6,51 @@ console.log(100);
 console.log("Hello World");
 
 console.log(20, "Hello", true); */
+
+// var, let, const
+
+let firstName = "Kenneth";
+let lastName = "WU";
+
+console.log(firstName, lastName, "sdfsdf");
+
+let age = 30;
+
+console.log(age);
+
+let nadfs = 5;
+
+age = 33;
+console.log("age", age);
+
+if (true) {
+  nadfs--;
+}
+console.log(nadfs);
+
+const arr1 = [1, 3, 5];
+
+arr1.push(7);
+console.log(arr1);
+
+// Cannot reassign
+// arr1 = [3, 2, 1];
+
+const me = {
+  name: "WU",
+  age: 32,
+};
+
+console.table(me);
+
+me.name = "John";
+me.email = "brad@gmail.com";
+
+console.table(me);
+
+let a, b, c;
+
+const d = 5,
+  e = 6,
+  f = 7;
+console.log(d, e, f);
