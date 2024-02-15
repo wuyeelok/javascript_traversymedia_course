2dde00cafeb50015706a189faf493cf1c0574d68
wuyeelok <wuyeelok@gmail.com>
2024-02-15 16:08:23 +0800

    Primitive type vs Object ref

--- sandbox/01-variables-data-types/05-stack-heap/script.js
+++ sandbox/01-variables-data-types/05-stack-heap/script.js
@@ -7,6 +7,7 @@ const person = {
   name: "Brad",
   age: 40,
 };
+console.table(person);
 
 let newName = name;
 newName = "Jonathan";
@@ -16,3 +17,34 @@ newPerson.name = "Bradley";
 
 console.log(name, newName); // John, Jonathan
 console.log(person, newPerson); // { name: 'Bradley', age: 40 }, { name: 'Bradley', age: 40 }
+
+const name2 = "Kenneth";
+const age2 = 32;
+
+let name3 = name2;
+// Before change
+console.log("name2 value is", name2);
+console.log("name3 value is", name3);
+
+name3 = "Tommy";
+
+// After change
+console.log("name2 value is", name2);
+console.log("name3 value is", name3);
+
+const car = {
+  horsePower: 400,
+  numOfDoor: 4,
+};
+
+const car2 = car;
+
+// Before change
+console.log("car value is", car);
+console.log("car2 value is", car2);
+
+car.numOfDoor = 2;
+
+// After change
+console.log("car value is", car);
+console.log("car2 value is", car2);
