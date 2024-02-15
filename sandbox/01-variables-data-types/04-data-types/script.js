fbd80fd9902b8188a5ce29194ff9091622066faf
wuyeelok <wuyeelok@gmail.com>
2024-02-15 15:23:09 +0800

    datatype intro

--- sandbox/01-variables-data-types/04-data-types/script.js
+++ sandbox/01-variables-data-types/04-data-types/script.js
@@ -1,5 +1,5 @@
 // String
-const firstName = 'Sara';
+const firstName = "Sara";
 
 // Number
 const age = 30;
@@ -16,7 +16,7 @@ const aptNumber = null;
 const score = undefined;
 
 // Symbol
-const id = Symbol('id');
+const id = Symbol("id");
 
 // BigInt
 const n = 9007199254740991n;
@@ -26,11 +26,12 @@ const n = 9007199254740991n;
 const numbers = [1, 2, 3, 4];
 
 const person = {
-  name: 'Brad',
+  name: "Brad",
 };
+console.log(typeof person);
 
 function sayHello() {
-  console.log('Hello');
+  console.log("Hello");
 }
 
 const output = sayHello;
@@ -39,6 +40,33 @@ console.log(output, typeof output);
 
 // More info on why typeof null == object
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null
-
+console.log("typeof null", typeof null);
 //  More info on the "function object" type
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+const middleName = "von";
+console.log(typeof middleName);
+
+// Both float and integer are number type
+const amount = 6.33;
+const numOfPeople = 4;
+console.log(typeof amount, typeof numOfPeople);
+
+console.log(typeof true);
+
+console.log(typeof undefined);
+
+// String
+const outoutput = "sara";
+console.log(outoutput, typeof outoutput);
+
+let unknown;
+console.log(unknown, typeof unknown);
+
+// Symbol
+const myId = Symbol("GGGG");
+console.log(myId, typeof myId);
+
+// BigInt
+const reallyBigNumber = -232432425298324n;
+console.log(reallyBigNumber, typeof reallyBigNumber);
