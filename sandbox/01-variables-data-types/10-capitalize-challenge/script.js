088df8517d338f4a15f287dba49e000645ee6186
wuyeelok <wuyeelok@gmail.com>
2024-02-20 21:35:37 +0800

    My solution on Cap 1st string letter

--- sandbox/01-variables-data-types/10-capitalize-challenge/script.js
+++ sandbox/01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,1 +1,19 @@
 console.log("10 Capitalize Challege Begins...");
+
+// Capitalize the 1st letter of a string
+const myString = "devleoper";
+console.log(`Original String: ${myString}`);
+
+const solution1 = myString.charAt(0).toUpperCase() + myString.slice(1);
+console.log(`Cap 1st letter solution 1: ${solution1}`);
+
+const solution2 = myString[0].toUpperCase() + myString.substring(1);
+console.log(`Cap 1st letter solution 2: ${solution2}`);
+
+const solution3 = `${myString.slice(0, 1).toUpperCase()}${myString.slice(1)}`;
+console.log(`Cap 1st letter solution 3: ${solution3}`);
+
+const solution4 = `${myString
+  .slice(-myString.length, -myString.length + 1)
+  .toUpperCase()}${myString.slice(-myString.length + 1)}`;
+console.log(`Cap 1st letter solution 4: ${solution4}`);
