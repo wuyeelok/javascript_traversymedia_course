89ed01ef438bd3688f287a71694a1248398ea61b
wuyeelok <wuyeelok@gmail.com>
2024-02-20 10:38:08 +0800

    brief intro to type coercion

---
+++ sandbox/01-variables-data-types/08-type-coercion/script.js
@@ -0,0 +1,39 @@
+/* let x;
+
+// Coerced to a string
+x = 5 + '5';
+
+x = 5 + Number('5');
+
+// Coerced to a number
+x = 5 * '5';
+
+// null is coerced to 0 as it is a `falsy` value
+x = 5 + null;
+
+x = Number(null);
+
+x = Number(true);
+x = Number(false);
+
+// true is coerced to a 1
+x = 5 + true;
+
+// false is coerced to 0 (falsy)
+x = 5 + false;
+
+// Undefined is coerced to 0 (falsy)
+x = 5 + undefined;
+
+console.log(x, typeof x);
+ */
+
+let z = 8;
+
+z += "3";
+
+z *= "5";
+
+z = 7 + false;
+
+console.log(z, typeof z);
