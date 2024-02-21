02809e7700b93bd4d2513c9d62695fbb165c4afd
wuyeelok <wuyeelok@gmail.com>
2024-02-21 13:22:45 +0800

    Intro to date and timestamp

---
+++ sandbox/01-variables-data-types/14-dates-and-times/script.js
@@ -0,0 +1,54 @@
+/* let d;
+
+// Get today's date and time
+d = new Date();
+
+// Set to a string
+d = d.toString();
+
+// Get a specific date
+// Important: the month is 0-based, so 0 is January and 11 is December
+d = new Date(2021, 0, 10, 12, 30, 0);
+
+// Pass in a string
+d = new Date('2021-07-10T12:30:00');
+d = new Date('07/10/2021 12:30:00');
+d = new Date('2022-07-10');
+d = new Date('07-10-2022');
+
+// https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
+
+// Get current timestamp
+d = Date.now();
+
+// Get the timestamp of a specific date
+d = new Date();
+d = d.getTime();
+d = d.valueOf();
+
+// Create a date from a timestamp
+d = new Date(1666962049745);
+
+// Convert from milliseconds to seconds
+d = Math.floor(Date.now() / 1000);
+
+console.log(d);
+ */
+
+let myD;
+
+myD = new Date().toString();
+
+console.log(myD, typeof myD);
+
+const yesterday = new Date(2024, 1, 20, 18, 58, 12);
+console.log(yesterday, typeof yesterday);
+
+const anotherDate = new Date("02/29/2024");
+console.log(anotherDate);
+
+const currentTimeS = Date.now();
+console.log(currentTimeS, typeof currentTimeS);
+
+const dd = new Date(1000);
+console.log(dd, typeof dd);
