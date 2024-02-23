bb1ba6874d55fb2241fe2551323f4262ebf0c09b
wuyeelok <wuyeelok@gmail.com>
2024-02-23 17:33:09 +0800

    Object naming and destruction

---
+++ sandbox/02-arrays-and-objects/07-destructuring-naming/script.js
@@ -0,0 +1,58 @@
+/* // Setting object properties with the same name as a variable
+const firstName = 'John';
+const lastName = 'Doe';
+const age = 30;
+
+const person = {
+  firstName,
+  lastName,
+  age,
+};
+
+console.log(person.age);
+
+// Destructuring object properties
+
+const todo = {
+  id: 1,
+  title: 'Take out trash',
+  user: {
+    name: 'John',
+  },
+};
+
+const {
+  id: todoId, // rename id to todoId
+  title,
+  user: { name }, // destructuring multiple levels
+} = todo;
+
+console.log(todoId);
+
+// Destructuring arrays & using the rest/spread operator
+const numbers = [23, 67, 33, 49, 52];
+
+const [first, second, ...rest] = numbers;
+
+console.log(first, second, rest);
+ */
+const firstName = "Kenneth";
+const lastName = "WU";
+const age = 33;
+
+const me = {
+  firstName,
+  lastName,
+  age,
+};
+
+console.log(me.lastName);
+
+const { firstName: myFirstName, age: myAge } = me;
+
+console.log(myFirstName, myAge);
+
+const colors = ["Red", "Blue", "Green", "Yello"];
+const [second, forth] = colors;
+
+console.log(colors, second, forth);
