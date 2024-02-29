70cd3aa3bb5f16b0ff9eb333414697306f45806b
wuyeelok <wuyeelok@gmail.com>
2024-02-29 08:45:57 +0800

    Finish calcalator challege

--- sandbox/04-logic-control-flow/04-calculator-challenge/script.js
+++ sandbox/04-logic-control-flow/04-calculator-challenge/script.js
@@ -1,1 +1,30 @@
 console.log("Calculator challege...");
+
+function calculator(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      result = "Invalid operator";
+  }
+
+  return result;
+}
+
+console.log(calculator(5, 2, "+"));
+console.log(calculator(5, 2, "-"));
+console.log(calculator(5, 2, "*"));
+console.log(calculator(5, 2, "/"));
+console.log(calculator(5, 2, "&"));
