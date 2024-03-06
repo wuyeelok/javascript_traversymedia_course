21f51238d74d9cc04d10d9d7d200891ee75b10d7
wuyeelok <wuyeelok@gmail.com>
2024-03-06 09:50:47 +0800

    Intro to setTimeout and clearTimeout

--- sandbox/09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ sandbox/09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -20,3 +20,25 @@ document.querySelector('#cancel').addEventListener('click', () => {
   console.log('Timer Cancelled');
 });
  */
+
+// Say Hello after 4 seconds
+setTimeout(function () {
+  console.log("Hello");
+}, 4000);
+
+function changeH1Text() {
+  const h1 = document.querySelector("h1");
+  h1.innerText = "Go Go Go From Callback";
+}
+
+// Call changeH1Text function after 3
+const timerId = setTimeout(changeH1Text, 3000);
+
+const cancelBtn = document.getElementById("cancel");
+cancelBtn.addEventListener("click", () => {
+  console.log(timerId);
+  clearTimeout(timerId);
+  console.log("Cancelled the Timer");
+});
+
+console.log("Good Morning!");
