ea61a864b1a1a60e04582f683c2da0664bedb7f0
wuyeelok <wuyeelok@gmail.com>
2024-03-06 16:14:46 +0800

    SetInterval: Able to change bg color and console log time

--- sandbox/09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ sandbox/09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -34,3 +34,70 @@ function stopChange() {
 document.getElementById('start').addEventListener('click', startChange);
 document.getElementById('stop').addEventListener('click', stopChange);
  */
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const startConsoleLogBtn = document.getElementById("startConsoleLog");
+const stopConsoleLogBtn = document.getElementById("stopConsoleLog");
+
+let myIntervalID;
+
+function printCurrentDatetime(v) {
+  const date = Intl.DateTimeFormat("en-GB", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }).format(Date.now());
+  console.log(v, date);
+}
+
+function startConsoleLog() {
+  if (!myIntervalID) {
+    myIntervalID = setInterval(printCurrentDatetime, 2000, `Now is`);
+  }
+}
+
+function stopConsoleLog() {
+  if (myIntervalID) {
+    clearInterval(myIntervalID);
+    myIntervalID = undefined;
+  }
+}
+
+startConsoleLogBtn.addEventListener("click", startConsoleLog);
+
+stopConsoleLogBtn.addEventListener("click", stopConsoleLog);
+
+let changeColorIntervalID;
+
+function startChangeBackGroundColor() {
+  // console.log(changeColorIntervalID);
+  if (!changeColorIntervalID) {
+    changeColorIntervalID = setInterval(changeBackGroundColor, 2000);
+  }
+  // console.log(changeColorIntervalID);
+}
+
+function stopChangeBackGroundColor() {
+  // console.log(changeColorIntervalID);
+  if (changeColorIntervalID) {
+    clearInterval(changeColorIntervalID);
+    changeColorIntervalID = undefined;
+  }
+  // console.log(changeColorIntervalID);
+}
+
+function changeBackGroundColor() {
+  if (document.body.style.backgroundColor !== "black") {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
+}
+
+startBtn.addEventListener("click", startChangeBackGroundColor);
+stopBtn.addEventListener("click", stopChangeBackGroundColor);
