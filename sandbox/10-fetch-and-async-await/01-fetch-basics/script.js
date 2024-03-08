98ec52b14e0ca853ae16f49ce8b35bedab46a159
wuyeelok <wuyeelok@gmail.com>
2024-03-08 23:50:04 +0800

    show test file

--- sandbox/10-fetch-and-async-await/01-fetch-basics/script.js
+++ sandbox/10-fetch-and-async-await/01-fetch-basics/script.js
@@ -14,3 +14,18 @@ fetch("../../../movies.json")
   })
   .then((data) => console.log("Movies:", data))
   .catch((error) => console.error("NOOOOOOOO", error));
+
+fetch("../../../test.txt")
+  .then((res) => {
+    console.log(res);
+    const contentType = res.headers.get("content-type");
+    console.log(contentType);
+    if (!contentType || !contentType.includes("text/plain")) {
+      throw new TypeError("Oops, we haven't got text!");
+    }
+    const myData = res.text();
+    console.log(myData);
+    return myData;
+  })
+  .then((data) => console.log("Text file test:", data))
+  .catch((error) => console.error("NOOOOOOOO", error));
