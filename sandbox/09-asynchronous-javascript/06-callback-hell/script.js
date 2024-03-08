9b726934af204e360fa514c78849556c20559e3d
wuyeelok <wuyeelok@gmail.com>
2024-03-08 14:20:22 +0800

    Try to refactor call backhell to Promise hell

--- sandbox/09-asynchronous-javascript/06-callback-hell/script.js
+++ sandbox/09-asynchronous-javascript/06-callback-hell/script.js
@@ -44,10 +44,45 @@ function getMyData(endpointURL, cb) {
 }
 
 // Callback hell when try to gurantee the order of getMyData
-getMyData("./movies.json", (data) => {
+/* getMyData("./movies.json", (data) => {
   console.log(data);
   getMyData("./actors.json", (data) => {
     console.log(data);
     getMyData("./directors.json", (data) => console.log(data));
   });
-});
+}); */
+
+function getMyDataPromise(endpointURL) {
+  return new Promise((resolve, reject) => {
+    const myXHR = new XMLHttpRequest();
+
+    myXHR.open("GET", endpointURL);
+
+    myXHR.addEventListener("error", () => console.log("Error while getMyData"));
+
+    myXHR.onreadystatechange = () => {
+      if (myXHR.readyState === 4 && myXHR.status === 200) {
+        const resp = JSON.parse(myXHR.responseText);
+        resolve(resp);
+      }
+    };
+    setTimeout(() => {
+      myXHR.send();
+    }, Math.floor(Math.random() * 3000) + 100);
+  });
+}
+
+// Try to refactor call backhell to Promise hell??
+getMyDataPromise("./movies.json")
+  .then((data) => {
+    console.log("Movie", data);
+    getMyDataPromise("./actors.json")
+      .then((data) => {
+        console.log("Actor", data);
+        getMyDataPromise("./directors.json")
+          .then((data) => console.log("Director", data))
+          .finally(() => console.log("Finish Director"));
+      })
+      .finally(() => console.log("Finish Actor"));
+  })
+  .finally(() => console.log("Finish Movie"));
