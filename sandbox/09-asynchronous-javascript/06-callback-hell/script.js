45ca609aad4d8b0a02203bf7caaffe025b6cd9ad
wuyeelok <wuyeelok@gmail.com>
2024-03-08 11:08:03 +0800

    No guarantee of order

--- sandbox/09-asynchronous-javascript/06-callback-hell/script.js
+++ sandbox/09-asynchronous-javascript/06-callback-hell/script.js
@@ -25,3 +25,25 @@ getData('./movies.json', (data) => {
 });
  */
 console.log("Call back hell!!!");
+
+function getMyData(endpointURL) {
+  const myXHR = new XMLHttpRequest();
+
+  myXHR.open("GET", endpointURL);
+
+  myXHR.addEventListener("error", () => console.log("Error while getMyData"));
+
+  myXHR.onreadystatechange = () => {
+    if (myXHR.readyState === 4 && myXHR.status === 200) {
+      console.log(JSON.parse(myXHR.responseText));
+    }
+  };
+  setTimeout(() => {
+    myXHR.send();
+  }, Math.floor(Math.random() * 3000) + 100);
+}
+
+// No guarantee of order
+getMyData("./movies.json");
+getMyData("./actors.json");
+getMyData("./directors.json");
