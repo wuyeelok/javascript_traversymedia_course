081b86d568465cddbe1a44d0ef36245078d754b2
wuyeelok <wuyeelok@gmail.com>
2024-03-08 15:12:45 +0800

    Setup my promise

--- sandbox/09-asynchronous-javascript/09-promise-chaining/script.js
+++ sandbox/09-asynchronous-javascript/09-promise-chaining/script.js
@@ -29,3 +29,21 @@ promise
   .then((x) => console.log('This will run no matter what', x));
  */
 console.log("Promise chain");
+
+const myProm = new Promise((res, rej) => {
+  setTimeout(() => {
+    let error = true;
+
+    if (!error) {
+      res({ goal: "WinRace", deadline: "30/10/2024" });
+    } else {
+      rej("Error: something was wrong");
+    }
+  }, 2000);
+});
+
+myProm
+  .then((target) => {
+    console.log(`Goal: ${target.goal}, deadline: ${target.deadline}`);
+  })
+  .catch((err) => console.log(err));
