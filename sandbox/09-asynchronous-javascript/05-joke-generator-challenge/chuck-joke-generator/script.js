3bf73e4ad9dd6fce72533fdceb9b89f02dda52e5
wuyeelok <wuyeelok@gmail.com>
2024-03-08 10:37:08 +0800

    Chunk joke: My solution

--- sandbox/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ sandbox/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -1,1 +1,30 @@
 console.log("Begin Challege...");
+
+// DOM Varaible
+const jokeDIV = document.getElementById("joke");
+const jokeBtn = document.getElementById("joke-btn");
+
+function showJoke(joke) {
+  jokeDIV.innerText = joke;
+}
+
+function getRandomJoke(callBackFunc) {
+  jokeDIV.innerText = "Loading...";
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+  xhr.addEventListener("error", () => (jokeDIV.innerText = "Error!"));
+
+  xhr.onreadystatechange = function () {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      const jokeObj = JSON.parse(xhr.responseText);
+      callBackFunc(jokeObj.value);
+    }
+  };
+
+  xhr.send();
+}
+
+jokeBtn.addEventListener("click", () => {
+  getRandomJoke(showJoke);
+});
