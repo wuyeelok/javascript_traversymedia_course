7cb0e13a51fa03fb1ed2a8bf04ec5659e9348add
wuyeelok <wuyeelok@gmail.com>
2024-03-08 23:55:20 +0800

    chain fetch api

--- sandbox/10-fetch-and-async-await/01-fetch-basics/script.js
+++ sandbox/10-fetch-and-async-await/01-fetch-basics/script.js
@@ -9,13 +9,29 @@ fetch("../../../movies.json")
       throw new TypeError("Oops, we haven't got JSON!");
     }
     const myData = res.json();
-    console.log(myData);
+
     return myData;
   })
-  .then((data) => console.log("Movies:", data))
+  .then((data) => {
+    console.log("Movies:", data);
+
+    return fetch("../../../test.txt");
+  })
+  .then((res) => {
+    console.log(res);
+    const contentType = res.headers.get("content-type");
+    console.log(contentType);
+    if (!contentType || !contentType.includes("text/plain")) {
+      throw new TypeError("Oops, we haven't got text!");
+    }
+    const myData = res.text();
+
+    return myData;
+  })
+  .then((data) => console.log("Text file test:", data))
   .catch((error) => console.error("NOOOOOOOO", error));
 
-fetch("../../../test.txt")
+/* fetch("../../../test.txt")
   .then((res) => {
     console.log(res);
     const contentType = res.headers.get("content-type");
@@ -29,3 +45,4 @@ fetch("../../../test.txt")
   })
   .then((data) => console.log("Text file test:", data))
   .catch((error) => console.error("NOOOOOOOO", error));
+ */
