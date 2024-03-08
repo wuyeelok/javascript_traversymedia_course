88b55d0e5d96c3ac4432a2f0ea5bab8da7535206
wuyeelok <wuyeelok@gmail.com>
2024-03-08 23:36:25 +0800

    basic calling of fetch api

--- sandbox/10-fetch-and-async-await/01-fetch-basics/script.js
+++ sandbox/10-fetch-and-async-await/01-fetch-basics/script.js
@@ -1,1 +1,11 @@
 console.log("Fetch basic");
+
+fetch("../../../movies.json")
+  .then((res) => {
+    console.log(res);
+    const myData = res.json();
+    console.log(myData);
+    return myData;
+  })
+  .then((data) => console.log("Movies:", data))
+  .catch((error) => console.error("NOOOOOOOO", error));
