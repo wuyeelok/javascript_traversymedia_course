bbab2c40929af803f0519d0c659e916696807467
wuyeelok <wuyeelok@gmail.com>
2024-03-09 21:51:55 +0800

    Script: Set up btn listeners

--- sandbox/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ sandbox/09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -3,14 +3,14 @@ console.log("Begin Challege...");
 // DOM Varaible
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("joke-btn");
+const jokeBtn2 = document.getElementById("joke-btn2");
+const jokeBtn3 = document.getElementById("joke-btn3");
 
 function showJoke(joke) {
   jokeEl.innerText = joke;
 }
 
 function generateJoke(callBackFunc) {
-  jokeEl.innerText = "Loading...";
-
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.chucknorris.io/jokes/random");
   xhr.addEventListener("error", () => (jokeEl.innerText = "Error!"));
@@ -29,9 +29,27 @@ function generateJoke(callBackFunc) {
   xhr.send();
 }
 
+function generateJoke2() {
+  console.log("generateJoke2");
+}
+
+function generateJoke3() {
+  console.log("generateJoke3");
+}
+
 jokeBtn.addEventListener("click", () => {
+  jokeEl.innerText = "Loading...";
   generateJoke(showJoke);
 });
+jokeBtn2.addEventListener("click", () => {
+  jokeEl.innerText = "Loading...";
+  generateJoke2(showJoke);
+});
+jokeBtn3.addEventListener("click", () => {
+  jokeEl.innerText = "Loading...";
+  generateJoke3(showJoke);
+});
+
 document.addEventListener(
   "DOMContentLoaded",
   () => (jokeEl.innerText = "Press the button below for a random joke")
