6eeb4677984520356a1571bdc4bc0d2008eda256
wuyeelok <wuyeelok@gmail.com>
2024-03-10 23:35:10 +0800

    Spinner effects

--- sandbox/10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ sandbox/10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -1,1 +1,9 @@
 console.log("Random User Mini Project");
+
+const generateBtn = document.getElementById("generate");
+const userDiv = document.getElementById("user");
+const spinnerDiv = document.querySelector(".spinner");
+
+generateBtn.addEventListener("click", () => {
+  spinnerDiv.classList.remove("hidden");
+});
