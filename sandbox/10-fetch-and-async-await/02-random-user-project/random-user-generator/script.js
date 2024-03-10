69c455f72f9c55695abce04157f0d9385d23edfc
wuyeelok <wuyeelok@gmail.com>
2024-03-10 23:49:11 +0800

    Error handling: Setup in case fail to fetch user

--- sandbox/10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ sandbox/10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -8,5 +8,21 @@ const api = "https://randomuser.me/api/";
 
 generateBtn.addEventListener("click", () => {
   spinnerDiv.classList.remove("hidden");
-  fetch(api).finally(() => spinnerDiv.classList.add("hidden"));
+  fetch(api)
+    .then((response) => {
+      if (!response.ok || response.status !== 200) {
+        throw new Error("Fail to get user!");
+      }
+    })
+    .catch((error) => {
+      let msg;
+
+      if (error.message.includes("Failed to fetch")) {
+        msg = "Error";
+      } else {
+        msg = error.message;
+      }
+      userDiv.innerText = msg;
+    })
+    .finally(() => spinnerDiv.classList.add("hidden"));
 });
