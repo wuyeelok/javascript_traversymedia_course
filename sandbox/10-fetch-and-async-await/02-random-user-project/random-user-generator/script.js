cc45481350f4b3eead69d7516b1c61a7b69acf4a
wuyeelok <wuyeelok@gmail.com>
2024-03-10 23:38:41 +0800

    Remove spinner after fetch random user

--- sandbox/10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ sandbox/10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -4,6 +4,9 @@ const generateBtn = document.getElementById("generate");
 const userDiv = document.getElementById("user");
 const spinnerDiv = document.querySelector(".spinner");
 
+const api = "https://randomuser.me/api/";
+
 generateBtn.addEventListener("click", () => {
   spinnerDiv.classList.remove("hidden");
+  fetch(api).finally(() => spinnerDiv.classList.add("hidden"));
 });
