6548671f1bc32b13a5b3f3f2d976a1ec56a1bd63
wuyeelok <wuyeelok@gmail.com>
2024-03-11 21:36:17 +0800

    Log 4 todos

--- sandbox/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ sandbox/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,1 +1,17 @@
 console.log("Typicode Todos Mini-Project");
+
+const api = "https://jsonplaceholder.typicode.com/todos";
+
+// show 4 todo
+function showTodo(url, howMany) {
+  fetch(`${url}?_limit=${howMany}`)
+    .then((resp) => resp.json())
+    .then((todos) => console.log(todos))
+    .catch((error) => console.error(error));
+}
+
+function init() {
+  showTodo(api, 4);
+}
+
+init();
