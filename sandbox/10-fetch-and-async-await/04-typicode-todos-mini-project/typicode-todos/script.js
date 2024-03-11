0c99606c1f5e1559d9073900d23bd887e461db16
wuyeelok <wuyeelok@gmail.com>
2024-03-11 21:44:34 +0800

    Able to show 5 todos

--- sandbox/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ sandbox/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -2,16 +2,33 @@ console.log("Typicode Todos Mini-Project");
 
 const api = "https://jsonplaceholder.typicode.com/todos";
 
-// show 4 todo
+const todoList = document.getElementById("todo-list");
+
+function createTodoDiv(todo) {
+  const divEle = document.createElement("div");
+  divEle.innerText = todo.title;
+
+  if (todo.completed) {
+    divEle.classList.add("done");
+  }
+
+  return divEle;
+}
+
 function showTodo(url, howMany) {
   fetch(`${url}?_limit=${howMany}`)
     .then((resp) => resp.json())
-    .then((todos) => console.log(todos))
+    .then((todos) => {
+      todos.forEach((todo) => {
+        const div = createTodoDiv(todo);
+        todoList.appendChild(div);
+      });
+    })
     .catch((error) => console.error(error));
 }
 
 function init() {
-  showTodo(api, 4);
+  showTodo(api, 5);
 }
 
 init();
