17e876bb5c1e8cb26b56d28d206a0b2fe895a7e5
wuyeelok <wuyeelok@gmail.com>
2024-03-13 13:48:41 +0800

    aync await with Promise.all

--- sandbox/09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
+++ sandbox/09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -30,7 +30,22 @@ function getMyDataProm(endpointURL) {
   });
 }
 
-console.log("promise vs callback hell");
+async function getMyDataProm3(endpointURL) {
+  try {
+    const resp = await fetch(endpointURL);
+    if (!resp.ok) {
+      throw new Error("Fail to get data");
+    }
+
+    const data = await resp.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/* console.log("Version 1: promise vs callback hell");
 
 getMyDataProm("../../../movies.json")
   .then((data) => {
@@ -42,4 +57,61 @@ getMyDataProm("../../../movies.json")
     return getMyDataProm("../../../directors.json");
   })
   .then((data) => console.log("My Directors", data))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err)); */
+
+async function getAllMyData() {
+  const movies = await getMyDataProm("../../../movies.json");
+  console.log("My Movies 2", movies);
+
+  const actors = await getMyDataProm("../../../actors.json");
+  console.log("My actors 2", actors);
+
+  const directors = await getMyDataProm("../../../directors.json");
+  console.log("My directors 2", directors);
+}
+
+// getAllMyData();
+
+async function getAllMyData3() {
+  const movies = await getMyDataProm3("../../../movies.json");
+  const actors = await getMyDataProm3("../../../actors.json");
+  const directors = await getMyDataProm3("../../../directors.json");
+  console.log(
+    "My movies v3",
+    movies,
+    "My actors v3",
+    actors,
+    "My directors v3",
+    directors
+  );
+}
+
+// getAllMyData3();
+
+async function getAllUsingPromiseAll() {
+  const [mRes, aRes, dRes] = await Promise.all([
+    fetch("../../../movies.json"),
+    fetch("../../../actors.json"),
+    fetch("../../../directors.json"),
+  ]);
+
+  const movies = await mRes.json();
+  const actors = await aRes.json();
+  const directors = await dRes.json();
+
+  console.log(movies, actors, directors);
+}
+
+// getAllUsingPromiseAll();
+
+async function getAllUsingPromiseAll2() {
+  const [movies, actors, directors] = await Promise.all([
+    fetch("../../../movies.json").then((resp) => resp.json()),
+    fetch("../../../actors.json").then((resp) => resp.json()),
+    fetch("../../../directors.json").then((resp) => resp.json()),
+  ]);
+
+  console.log(movies, actors, directors);
+}
+
+getAllUsingPromiseAll2();
