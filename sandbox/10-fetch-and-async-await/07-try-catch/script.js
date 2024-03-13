760c6bde59e9eae18bae88efd2c948bec0f673cf
wuyeelok <wuyeelok@gmail.com>
2024-03-13 12:33:10 +0800

    basic try catch

--- sandbox/10-fetch-and-async-await/07-try-catch/script.js
+++ sandbox/10-fetch-and-async-await/07-try-catch/script.js
@@ -1,1 +1,22 @@
 console.log("Try catch");
+
+try {
+  console.log(abc);
+} catch (error) {
+  console.log(`Error: ${error}`);
+}
+
+function trible(num) {
+  if (isNaN(num)) {
+    throw new Error(`${num} is not a number`);
+  }
+
+  return num * 3;
+}
+
+try {
+  const y = trible("ssdf");
+  console.log(y);
+} catch (err) {
+  console.log(err);
+}
